ee91ef847ee805bdb4b9ffd0ce601136538a5ee7
Josue Fdz <josue.fernandez@netpay.com.mx>
2022-11-29 22:26:07 -0600

    Add tensoflow

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import {Subject, Observable} from 'rxjs';
 import {WebcamImage, WebcamInitError, WebcamUtil} from 'ngx-webcam';
 import { GeolocationService } from 'src/services/geolocation.service';
 import * as L from 'leaflet';
+import * as mobilenet from '@tensorflow-models/mobilenet';
 
 @Component({
   selector: 'app-root',
@@ -10,12 +11,17 @@ import * as L from 'leaflet';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
+  @ViewChild('video') video: ElementRef;
   title = 'camaraapp';
   latitude: any;
   longitude: any;
 
   map: any;
   marker: L.Marker<any>;
+
+  model: any;
+  loading: boolean;
+  predictions: any;
 
   constructor(private locationService: GeolocationService){
 
@@ -30,12 +36,50 @@ export class AppComponent implements OnInit {
   // Cada Trigger para una nueva captura o foto 
   public trigger: Subject<void> = new Subject<void>();
 
-  ngOnInit(): void {
+  async ngOnInit() {
+    this.loading = true;
+    this.model = await mobilenet.load();
+    this.loading = false;
+  }
+
+  private initMap(): void {
+    this.locationService.getPosition().then(pos => {
+      this.latitude = pos.lat;
+      this.longitude = pos.lng;
+      this.map = L.map('map').setView([this.latitude, this.longitude], 13);
+      L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+          maxZoom: 19,
+          attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+      }).addTo(this.map);
+      this.marker = L.marker([this.latitude, this.longitude]).addTo(this.map);
+    });
+    console.log(this.map);
+  }
+
+  ngAfterViewInit() {
+    const vid = this.video.nativeElement;
+    this.initMap();
+
+    if (navigator.mediaDevices.getUserMedia) {
+      navigator.mediaDevices.getUserMedia({ video: true })
+        .then((stream) => {
+          vid.srcObject = stream;
+          console.log(vid.srcObject);
+        })
+        .catch((err0r) => {
+          console.log('Something went wrong!');
+        });
+    }
+
+    setInterval(async () => {
+      this.predictions = await this.model.classify(this.video.nativeElement);
+   }, 3000);
   }
 
   public triggerCaptura(): void {
-    this.getLocation()
-    this.trigger.next();
+    // this.takeFoto();
+    // this.getLocation()
+    // this.trigger.next();
   }
 
   public toggleWebcam(): void {
@@ -54,6 +98,26 @@ export class AppComponent implements OnInit {
   public get triggerObservable(): Observable<void> {
     return this.trigger.asObservable();
   }
+
+  // takeFoto() {
+  //   console.log(this.video);
+  //   const vid = this.video.nativeElement;
+
+  //   if (navigator.mediaDevices.getUserMedia) {
+  //     navigator.mediaDevices.getUserMedia({ video: true })
+  //       .then((stream) => {
+  //         vid.srcObject = stream;
+  //         console.log(vid.srcObject);
+  //       })
+  //       .catch((err0r) => {
+  //         console.log('Something went wrong!');
+  //       });
+  //   }
+
+  //   this.predictions = this.model.classify(this.video.nativeElement);
+  // //   setInterval(() => {
+  // //  }, 3000);
+  // }
 
   getLocation() {
     this.locationService.getPosition().then(pos => {
